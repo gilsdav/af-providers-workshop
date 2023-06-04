1537150ecac28f74507d5520efcaa4ecb5ffeb48
YordiDavid <y.david@afelio.be>
2023-06-04 20:59:58 +0200

    move types in types folder

--- src/app/modules/custom-module/custom-module.module.ts
+++ src/app/modules/custom-module/custom-module.module.ts
@@ -2,32 +2,32 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { customModuleContainers } from './containers';
 import { CustomModuleRoutingModule } from './custom-module-routing.module';
-import { Config, MODULE_CONFIG } from './models/config.type';
+import { Config, MODULE_CONFIG } from './models/types/config.type';
 import { defaultConfig } from './default-config.constant';
 
 
 
 @NgModule({
-	declarations: [
-		...customModuleContainers
-	],
-	imports: [
-		CommonModule,
-		CustomModuleRoutingModule
-	]
+    declarations: [
+        ...customModuleContainers
+    ],
+    imports: [
+        CommonModule,
+        CustomModuleRoutingModule
+    ]
 })
 export class CustomModuleModule {
-	public static forRoot(config?: Config): ModuleWithProviders<CustomModuleModule> {
+    public static forRoot(config?: Config): ModuleWithProviders<CustomModuleModule> {
 
-		const computedConfig: Config = config ? { ...defaultConfig, ...config } : defaultConfig;
+        const computedConfig: Config = config ? { ...defaultConfig, ...config } : defaultConfig;
 
-		const module: ModuleWithProviders<CustomModuleModule> = {
-			ngModule: CustomModuleModule,
-			providers: [
-				{ provide: MODULE_CONFIG, useValue: computedConfig }
-			]
-		}
+        const module: ModuleWithProviders<CustomModuleModule> = {
+            ngModule: CustomModuleModule,
+            providers: [
+                { provide: MODULE_CONFIG, useValue: computedConfig }
+            ]
+        }
 
-		return module;
-	}
+        return module;
+    }
 }
