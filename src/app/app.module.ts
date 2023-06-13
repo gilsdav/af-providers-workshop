5d415a29f18d767ee65e79129d03340ccce3dbd2
YordiDavid <y.david@afelio.be>
2023-06-13 15:06:55 +0200

    undelete comment imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomModuleModule } from './modules/custom-module/custom-module.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AlternativeUserService } from './services/alternative-user.service';
 import { LoggerModule } from './modules/logger-module/logger.module';
 import { DynamicComponentModule } from './modules/dynamic-component-module/dynamic-component.module';
 import { TestComponent } from './components/test/test.component';
 import { AltAuthService } from './services/alternative-auth.service';
+import { AlternativeGuard } from './guards/alternative-guard.guard';
 
 @NgModule({
 	declarations: [
